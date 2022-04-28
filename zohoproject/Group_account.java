2e1e5fd4c74c0c5121f84b0d352e09316f43d125
JD <68143125+janabeep@users.noreply.github.com>
2022-04-28 14:54:07 +0530

    Delete zohoproject directory

--- zohoproject/Group_account.java
+++
@@ -1,9 +0,0 @@
-package zohoproject;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-
-public class Group_account {
-	HashMap<String, HashMap<String,Double> > grpaccount;
-	HashMap<String, ArrayList<String>> grpdetail;
-}
